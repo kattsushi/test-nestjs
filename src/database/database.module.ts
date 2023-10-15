d5947be41035397bcc1d273fa9cb0d5cc6f5984f
Andres Jimenez <andresdavid@outlook.com>
2023-10-15 12:30:49 -0600

    use mongodb uri from env var

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
+import mongoose from 'mongoose';
+
+export const databaseProviders = [
+  {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (config: ConfigService): Promise<typeof mongoose> =>
+      await mongoose.connect(config.get('MONGODB_URI')),
+  },
+];
 
 @Module({
+  imports: [ConfigModule],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
